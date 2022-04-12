db70fa65e368b3ed5f847bc4cefbf7c9cbf4e435
nologyRob <rob.hooper@nology.io>
2022-04-12 14:20:25 +0100

    Working on Day 02 Branch

---
+++ src/com/company/Challenge.md
@@ -0,0 +1,6 @@
+## Challenge
+
+- Create a Bank class.
+- Create some field members (variables) that a bank would have.
+- e.g. Bank name, Bank Location, Bank Staff Members (use Math.Random for this)
+- Use the Switch statement and user Input in Main to create a new Bank object. 
--- src/com/company/Customer.java
+++ src/com/company/Customer.java
@@ -1,20 +1,51 @@
 package com.company;
 
-import java.util.ArrayList;
-
 public class Customer {
 
-    private String name;
+    //Opportunity to learn about field members
+    private String firstName;
+    private String lastName;
+    private double initialAmount;
 
-    public Customer(String name, double initialAmount) {
-        this.name = name;
+    //Opportunity to learn about Math.random
+    //Opportunity to learn about casting
+    //Opportunity to learn about encapsulation
+    //Opportunity to learn about modifiers
+    private final int numberOfAccounts = (int) (Math.random()*10);
+
+
+    //Opportunity to learn about constructors
+    public Customer(String firstName, String lastName, double initialAmount) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.initialAmount = initialAmount;
     }
 
-    public String getName() {
-        return name;
+
+    //Opportunity to learn about getters/setters
+
+    public String getFirstName() {
+        return firstName;
     }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
+    public String getLastName() {
+        return lastName;
+    }
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
+    public int getNumberOfAccounts() {
+        return numberOfAccounts;
+    }
+
+    public double getInitialAmount() {
+        return initialAmount;
+    }
 }
