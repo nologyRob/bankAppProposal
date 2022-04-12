0b697fa302766e5500879280611a2952bc597c3e
nologyRob <rob.hooper@nology.io>
2022-04-12 14:29:45 +0100

    Working on Day 03 Branch

--- src/com/company/Bank.java
+++ src/com/company/Bank.java
@@ -2,5 +2,33 @@ package com.company;
 
 public class Bank {
 
+    private String bankName;
+    private String bankLocation;
+    private final int bankStaffMembers = (int) (Math.random()*20);
+
+    public Bank(String bankName, String bankLocation) {
+        this.bankName = bankName;
+        this.bankLocation = bankLocation;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getBankLocation() {
+        return bankLocation;
+    }
+
+    public void setBankLocation(String bankLocation) {
+        this.bankLocation = bankLocation;
+    }
+
+    public int getBankStaffMembers() {
+        return bankStaffMembers;
+    }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,6 @@ import java.util.Scanner;
 public class Main {
 
     private static Scanner scanner = new Scanner(System.in);
-    private static Bank bank = new Bank();
-
 
     public static void main(String[] args) {
 
@@ -16,7 +14,7 @@ public class Main {
 
         System.out.println("\n Choose one of the following! : \n");
         System.out.println("0. To ShutDown.");
-        System.out.println("1. To Add a name to your Bank.");
+        System.out.println("1. To create a Bank.");
         System.out.println("2. To Add branch to a Bank.");
         System.out.println("3. To Add a Customer to a Bank's Branch.");
 
@@ -30,7 +28,14 @@ public class Main {
                     quit = true;
                     break;
                 case 1:
-                    System.out.println("\n Let's add a name to your bank!");
+                    System.out.println("\n Let's create a bank!");
+                    System.out.println("\n What is the name of the bank that you would like to create?");
+                    String bankName = scanner.nextLine();
+                    System.out.println("\n What is the location of the bank that you would like to create?");
+                    String bankLocation = scanner.nextLine();
+                    Bank bank = new Bank(bankName, bankLocation);
+                    System.out.println("Congratulations, you have added a new bank with the name of " + bank.getBankName() + " and this bank is located at " + bank.getBankLocation() + ".  This bank has " + bank.getBankStaffMembers() + " employees");
+
                     break;
                 case 2:
                     System.out.println("\n Let's add a branch!");
